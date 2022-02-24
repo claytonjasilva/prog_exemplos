05ea13fe3c3a3563f5ad0711c9fa004ddba8941a
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-02-24 12:32:47 -0300

    Create oimundo.py

---
+++ oimundo.py
@@ -0,0 +1,16 @@
+#Vamos começar escrevendo o primeiro código em Python...
+#...após ter atribuído um nome para seu arquivo o replit o salvará como '<nome>.py...'
+#...após abrir o arquivo, o código conterá uma função principal chamada 'main' com uma extensão '.py' - ou seja, main.py
+#O código-fonte do seu programa é organizado em linhas numeradas,...
+#...em cada uma será escrito um comando da linguagem
+#Neste exemplo usa-se o comando # na primeira linha: representa um comentário 
+
+#A linha acima está em branco e também não é convertida pelo interpretador...
+#...no programa a ser executado
+
+#Na linha seguinte está apresentado uma instrução do Python 'print("texto")'
+print("Oi, mundo!")
+print("primeiro programa em Python...") #string - cadeia de caracteres entre ""
+print('...aprendendo o que é string ') #ou entre ''
+
+#Você pode ver mais em <https://docs.python.org/3/tutorial/introduction.html>
