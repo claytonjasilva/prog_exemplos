295fdcce72dd5e66bd3b169b6a8cda15369840cf
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-02-25 19:35:06 -0300

    Create conceitosbas_3.py

---
+++ conceitosbas_3.py
@@ -0,0 +1,12 @@
+#============================================================================
+# Nome        : copiaInOut.py
+# Autor       : clayton j a silva
+# Versão      :
+# Copyright   : clayton j a silva
+# Descrição   : Copia entrada para saida
+#============================================================================
+
+# copia entrada para saída
+
+valor_lido = input("Digite a linha: ") #A sequencia de caracteres é do tipo string - cadeia de caracteres - comando de entrada
+print(valor_lido)
