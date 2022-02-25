dd39538b61cac6b5d6e1c4893401a4e4bd0bddbd
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-02-25 19:36:29 -0300

    Update conceitosbas_3.py

--- conceitosbas_3.py
+++ conceitosbas_3.py
@@ -1,12 +1,10 @@
+#
 #============================================================================
-# Nome        : copiaInOut.py
 # Autor       : clayton j a silva
-# Versão      :
-# Copyright   : clayton j a silva
-# Descrição   : Copia entrada para saida
-#============================================================================
-
-# copia entrada para saída
+# Descrição   : exemplo tipos, operadores, entrada/saída 
+# ============================================================================
+#
+# O programa copia uma entrada digitada pelo usuário para a saída
 
 valor_lido = input("Digite a linha: ") #A sequencia de caracteres é do tipo string - cadeia de caracteres - comando de entrada
 print(valor_lido)
