22de0fae93397b9d44b73617e906c6d1553d4841
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-02-26 11:41:02 -0300

    Create condicional_2.py

---
+++ condicional_2.py
@@ -0,0 +1,27 @@
+#============================================================================
+# Nome        : testaString.py
+# Autor       : clayton j a silva
+# Versão      : 0
+# Copyright   : clayton j a silva
+# Descrição   : Realiza testes em uma frase
+#============================================================================
+
+print("O programa calcula as raizes de um polinomio do segundo grau na forma a.x2+b.x+c")
+
+#Leitura dos coeficientes do polinomio
+a = float(input("Digite o valor do coeficiente 'a' "))
+b = float(input("Digite o valor do coeficiente 'b' "))
+c = float(input("Digite o valor do coeficiente 'c' "))
+delta = b**2 - 4*a*c
+if (delta==0):
+  r1=-b/(2*a)
+  r2=r1
+  print("A raiz 1 e ",r1)
+  print("A raiz 2 e ",r2)
+if (delta<0):
+  print("As raizes sao imaginarias")
+if (delta>0):
+  r1 = (-b + delta**0.5)/(2*a)
+  r2 = (-b - delta**0.5)/(2*a)
+  print("A raiz 1 e ",r1)
+  print("A raiz 2 e ",r2)
