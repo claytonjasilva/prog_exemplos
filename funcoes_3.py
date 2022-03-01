4f83095ede0368b0a1321fbad597e876a166cbd1
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-03-01 18:25:27 -0300

    Create funcoes_3.py

---
+++ funcoes_3.py
@@ -0,0 +1,9 @@
+def maior(a,b): #A função não precisa retornar um valor
+  if (a>b):
+    print(a)
+  else:
+    print(b)
+
+x=int(input("Digite o primeiro numero: "))
+y=int(input("Digite o segundo numero: "))
+maior(x,y)
