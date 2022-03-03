8507d39cb50fbbd3d92ddecd0bf594f4bb58fcc3
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-03-02 22:28:28 -0300

    Create pacotes.py

---
+++ pacotes.py
@@ -0,0 +1,14 @@
+import statistics
+import matplotlib.pyplot as plt
+
+lista=[] #Cria uma lista vazia...estudaremos lista daqui a pouco
+print("O programa lê e calcula a média da idade de um grupo de alunos")
+for i in range(5):
+  print("aluno ",i) 
+  idade=int(input("idade \n")) #'\n' realiza uma quebra de linha
+  lista.append(idade) #Inlcui o elemento lido na lista
+print("relação de idades \n",lista)
+print("idade média: \n",statistics.mean(lista))
+x=[0,1,2,3,4]
+plt.plot(x,lista)
+plt.show()
