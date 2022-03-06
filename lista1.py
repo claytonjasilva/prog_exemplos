8f2a5f616b544ebca42278de165bf3914b97a63e
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-03-06 16:44:42 -0300

    Create lista1.py

---
+++ lista1.py
@@ -0,0 +1,17 @@
+#O programa ilustra a utilizacao de listas 
+#Elaborado por Clayton J A Silva
+#Em 9fev2022
+
+print("O programa le as notas de um grupo de N alunos...")
+print("determina a maior das notas e apresenta a porcentagem de cada nota em relacao a maior")
+N = int(input("Defina o numero de alunos "))
+nota = [] #Cria a lista vazia
+cont = 0
+maior = 0
+while (cont<N):
+  nota.append(float(input("Digite a nota ")))
+  if (nota[cont]>maior):
+    maior = nota[cont]
+  cont += 1
+for num in range(0,N):
+  print("Aluno ",num+1," ",nota[num]/maior*100) #Admite-se que pelo menos uma nota e maior do que 0
