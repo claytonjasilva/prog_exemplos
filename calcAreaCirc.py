bd5678bec5ef73eb55fa3bcd51005ae8026af9e0
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-03-20 09:11:35 -0300

    Create calcAreaCirc.py

---
+++ calcAreaCirc.py
@@ -0,0 +1,19 @@
+#=========================================================
+# calcAreaCirc.py
+# O programa calcula a área e o perímetro de um círculo
+# Clayton J A Silva
+# 19mar2022
+
+import math # math é uma biblioteca de funções matemáticas
+            # ver em <https://docs.python.org/pt-br/3/library/math.html>
+
+# Leitura dos dados de entrada e atribuição às variáveis do tipo ponto flutuante
+raio=float(input("Digite o raio do círculo: "))
+
+# Calcula a área e o perímetro
+area=math.pi*raio**2 # Usa a constante math.pi
+perimetro=2*math.pi*raio
+
+# Escreve as respostas
+print("A área do círculo de raio ",raio, " e \n",round(area,1)) # O caractere especial '\n' quebra linha
+print("O perímetro do círculo de raio ",raio, " e \n",round(perimetro,1))
