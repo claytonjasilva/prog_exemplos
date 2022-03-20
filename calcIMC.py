000332723c103dc16048595d33e3a7b99b22aa62
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-03-20 09:26:35 -0300

    Create calcIMC.py

---
+++ calcIMC.py
@@ -0,0 +1,15 @@
+#=========================================================
+# calcIMC.py
+# O programa calcula o índice de massa corpórea de uma pessoa
+# Clayton J A Silva
+# 19mar2022
+
+# Leitura dos dados de entrada e atribuição às variáveis do tipo ponto flutuante
+peso=float(input("Digite o peso da pessoa: \n")) # O caractere especial '\n' quebra linha
+altura=float(input("Digite a sua altura: \n"))
+
+# Calcula o imc
+imc=peso/altura**2
+
+# Escreve as raízes
+print("O IMC da pessoa é ",round(imc,1))
