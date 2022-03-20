56b872545b36bcb5898606c2cf19db1ec4af4d9f
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-03-20 09:24:59 -0300

    Update calcArea2.py

--- calcArea2.py
+++ calcArea2.py
@@ -10,21 +10,23 @@ import math # math é uma biblioteca de funções matemáticas
 area=0 # Atribui um valor incial para a área
 
 # Seleciona a figura
-op=input("Digite a figura desejada (1) Circulo, (2) Triangulo, (3) Retangulo: ")
+op=input('Digite a figura desejada (1) Circulo, (2) Triangulo, (3) Retangulo: ')
 
 if (op=='1'): #As cadeias de caracteres também podem ser delimitadas por aspas simples
-    raio=float(input("Digite o raio do círculo: "))
+    raio=float(input('Digite o raio do círculo: '))
     area=math.pi*raio**2 # Usa a constante math.pi
 elif (op=='2'):
-    base=float(input("Digite a base do triangulo: "))
-    altura=float(input("Digite a altura do triangulo: "))
+    base=float(input('Digite a base do triangulo: '))
+    altura=float(input('Digite a altura do triangulo: '))
     area=base*altura/2
 elif (op=='3'):
-    lado1=float(input("Digite o lado 1 do retangulo: "))
-    lado2=float(input("Digite o lado 2 do retangulo: "))
+    lado1=float(input('Digite o lado 1 do retangulo: '))
+    lado2=float(input('Digite o lado 2 do retangulo: '))
     area=lado1*lado2
 else:
-   print("Você digitou uma figura inválida")
+   print('Você digitou uma figura inválida')
 
 if (area!=0):
-   print("A área da figura é ",round(area,2)) 
+   print('A área da figura é ',round(area,2)) 
+
+
