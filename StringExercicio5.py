c07b88088771a7ffe898b156b7425687d76189a2
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-05-24 22:43:08 -0300

    Create StringExercicio5.py

---
+++ StringExercicio5.py
@@ -0,0 +1,13 @@
+#5. Elaborar um script em Python que leia uma relação de nomes de alunos 
+#Após cadastrar a relação o usuário poderá digitar uma letra e o 
+#programa retornará todos os nomes iniciados com a letra digitada.
+
+N = int(input('Digite o tamanho da relação: '))
+nome = []
+for i in range(N): # leitura e cadastro da relação
+  nome.append(input('Digite o nome: '))
+  
+letra = input('Digite a letra: ')
+for i in range(N):
+  if nome[i][0]==letra: # nome[i] é um string, logo nome[i][0] é a primeira letra da string nome[i]
+    print(nome[i])
