f592421b3b75f06cb2f47f4a83a734ac323fee59
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-05-24 22:21:13 -0300

    Create StringExercicio1.py

---
+++ StringExercicio1.py
@@ -0,0 +1,9 @@
+#1. Elaborar um script em Python que permita ler uma palavra digitada pelo usuário.
+#O programa deverá escrever todas as letras da palavra diferentes da letra ‘a’. 
+
+palavra = input('Digite a palavra: ') # Lê a palavra
+N = len(palavra) # Determina o tamanho
+for i in range(N):
+  if palavra[i]!='a': # Uma string é uma sequência imutável - cada letra corresponde a uma posição ordenada
+    print(palavra[i]) # Escreve a letra da palavra diferente de 'a'
+
