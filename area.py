ac2319782db67818cd4696a2aa54a907ac3cd473
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-09 17:57:30 -0300

    Create area.py

---
+++ area.py
@@ -0,0 +1,5 @@
+# O símbolo hash(cerquilha ou jogo da velha) orienta o interpretador Python a ignorar os caracteres da linha
+# Usado para definir COMENTÁRIOS - esclarecem trechos do código, facilitando a sua LEGIBILIDADE
+# Estão relacionados diretamente com a QUALIDADE do código, pois facilitam a manutenção
+
+print(50*15,' metros quadrados') # A função print da linguagem Python admite dois ou mais argumentos separados por vírgula
