b183eb8a22b3e3bd6a314ef21c4a97c9558586c6
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-09 17:59:45 -0300

    Create volta.py

---
+++ volta.py
@@ -0,0 +1,6 @@
+# O símbolo hash(cerquilha ou jogo da velha) orienta o interpretador Python a ignorar os caracteres da linha
+# Usado para definir COMENTÁRIOS - esclarecem trechos do código, facilitando a sua LEGIBILIDADE
+# Estão relacionados diretamente com a QUALIDADE do código, pois facilitam a manutenção
+
+print(2300//400,' voltas completas') # A função print da linguagem Python admite dois ou mais argumentos separados por vírgula
+print('O corredor percorreu além da volta completada ',2300%400,' metros') # O operador % devolve o resto da divisão inteira
