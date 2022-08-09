61dafa8f2481fc52fb71c5d8185d084023924c74
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-09 18:04:21 -0300

    Create nota2.py

---
+++ nota2.py
@@ -0,0 +1,15 @@
+# O símbolo hash(cerquilha ou jogo da velha) orienta o interpretador Python a ignorar os caracteres da linha
+# Usado para definir COMENTÁRIOS - esclarecem trechos do código, facilitando a sua LEGIBILIDADE
+# Estão relacionados diretamente com a QUALIDADE do código, pois facilitam a manutenção
+
+# Em Python, os nomes de variáveis também devem obedecer a algumas regras simples: 
+# Um nome de variável é uma sequência de letras (a → z, A → Z) e números (0 → 9), 
+# que devem sempre começar com uma letra. Apenas letras comuns são permitidas. 
+# Letras acentuadas, cedilhas, espaços, caracteres especiais como $, #, @, etc.
+# são proibidos, exceto para o caractere _ (sublinhado/underline).
+
+ap1 = 6.5
+ap2 = 8.3
+ac = 6.9
+nf = 0.4*ap1+0.4*ap2+0.2*ac
+print(nf) 
