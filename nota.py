34dcb8ce81a39bf217351c09d492c35240ee7dd7
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-09 18:02:28 -0300

    Create nota.py

---
+++ nota.py
@@ -0,0 +1,5 @@
+# O símbolo hash(cerquilha ou jogo da velha) orienta o interpretador Python a ignorar os caracteres da linha
+# Usado para definir COMENTÁRIOS - esclarecem trechos do código, facilitando a sua LEGIBILIDADE
+# Estão relacionados diretamente com a QUALIDADE do código, pois facilitam a manutenção
+
+print(0.4*6.5+0.4*8.3+0.2*6.9) # Observar que o operador * tem precedência - é realizado antes - sobre +
