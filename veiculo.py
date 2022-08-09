1143c48dd351827b52d78a9f863101ccad9064c3
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-09 18:01:09 -0300

    Create veiculo.py

---
+++ veiculo.py
@@ -0,0 +1,5 @@
+# O símbolo hash(cerquilha ou jogo da velha) orienta o interpretador Python a ignorar os caracteres da linha
+# Usado para definir COMENTÁRIOS - esclarecem trechos do código, facilitando a sua LEGIBILIDADE
+# Estão relacionados diretamente com a QUALIDADE do código, pois facilitam a manutenção
+
+print(19.5/2*50,' quilômetros') # Observer que a parte inteira do número em ponto flutuante é separa por ponto decimal da parte fracionária
