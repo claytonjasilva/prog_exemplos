26757ad6fa41ec25b36ac33f051f79240da8904d
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-12 15:55:58 -0300

    Update maior2num.py

--- maior2num.py
+++ maior2num.py
@@ -2,14 +2,17 @@
 # maior2num.py
 # O programa lê dois números, determina e escreve o maior entre eles
 # Clayton J A Silva
-# 26mar2022
+# 12ago2022
 
+# Leitura dos números
 num1=float(input('Digite o número 1: '))
 num2=float(input('Digite o número 2: '))
 
-if (num1<num2):
-    print('O maior número é ',num2)
-elif (num1==num2):
-    print('Os números são iguais ')
-else:
+# Determinação e apresentação do maior entre eles
+if (num1>num2):
     print('O maior número é ',num1)
+else: 
+    if (num2>num1):
+        print('O maior número é ',num2)
+    else:
+        print('Os números são iguais')
