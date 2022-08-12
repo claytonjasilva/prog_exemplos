f686a176a0a49b8526b936957a3e6ba49f599353
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-12 16:40:14 -0300

    Update maior3id.py

--- maior3id.py
+++ maior3id.py
@@ -1,28 +1,32 @@
-#=========================================================
+# =========================================================
 # maior3id.py
 # O programa lê nome e idade de 3 animais
+# calcula a idade do mais velho e escreve o seu nome
 # Clayton J A Silva
-# 26mar2022
+# 12ago2022
 
 
-#animal 1
-nome=input('Digite o nome do animal ') # entrada de dados
-idade=int(input('Digite a idade do animal '))
-maior_idade=idade
-nome_maior=nome
+# Dados do animal 1
+nome = input('Digite o nome do animal ')
+idade = int(input('Digite a idade do animal '))
 
-#animal 2
-nome=input('Digite o nome do animal ') # entrada de dados
-idade=int(input('Digite a idade do animal '))
-if (idade>maior_idade): # testa se a idade é maior do que maior valor anterior
+# Inicialização dos dados do mais velho
+maior_idade = idade
+nome_maior = nome # Além da idade é necessário guardar também o nome do animal mais velho
+
+# Dados do animal 2
+nome = input('Digite o nome do animal ')  # Observe que os dados do animal anterior são perdidos
+idade = int(input('Digite a idade do animal '))
+if idade > maior_idade:  # testa se a idade é maior do que a idade do primeiro animal
+                         # substitui o valor anterior somente se o animal 2 for mais velho
     maior_idade = idade
     nome_maior = nome
 
-#animal 3
-nome=input('Digite o nome do animal ') # entrada de dados
-idade=int(input('Digite a idade do animal '))
-if (idade>maior_idade): # testa se a idade é maior do que maior valor anterior
+# Dados do animal 3
+nome = input('Digite o nome do animal ')
+idade = int(input('Digite a idade do animal '))
+if idade > maior_idade:  # testa se a idade é maior do que o mais velho dentre os 2 anteriores
     maior_idade = idade
     nome_maior = nome
 
-print('O nome do animal mais velho é: ',nome_maior, '\n sua idade é ',maior_idade)
+print('O nome do animal mais velho é: ', nome_maior, '\n sua idade é ', maior_idade)
