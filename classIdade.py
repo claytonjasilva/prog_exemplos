2cf0dede910d2708c099fc8799d3b5d5182929b4
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-12 16:01:51 -0300

    Update classIdade.py

--- classIdade.py
+++ classIdade.py
@@ -2,13 +2,15 @@
 # classIdade.py
 # O programa lê a idade de uma pessoa e classifica
 # Clayton J A Silva
-# 26mar2022
+# 12ago2022
 
+# Leitura da idade
 idade=int(input('Digite a idade: '))
 
+# Classificação da idade lida
 if (idade<=10):
     print('CRIANÇA')
-elif (idade<18):
+elif (idade<18): # Utiliza-se a expressão condicional elif, que é a contração de else+if = elif
     print('ADOLESCENTE')
 else:
     print('ADULTO')
