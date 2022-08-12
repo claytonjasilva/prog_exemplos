68dca648dd1bcc6206edfe46a988a6744d855083
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-12 16:23:56 -0300

    Update calcIMC3.py

--- calcIMC3.py
+++ calcIMC3.py
@@ -1,36 +1,37 @@
-#=========================================================
+# =========================================================
 # calcIMC3.py
-# O programa lê nome, peso e altura de pessoas
+# O programa lê nome, peso e altura de 3 pessoas
+# escreve somente nome e imc das pessoas abaixo ou acima de dois limites
 # Clayton J A Silva
-# 26mar2022
+# 12ago2022
 
+# Leitura dos dados da primeira pessoa
+# Dados da pessoa 1
+nome = input('Digite o nome da pessoa ')  # entrada de dados
+peso = float(input('Digite o peso da pessoa '))
+altura = float(input('Digite a altura da pessoa '))
 
-#pessoa 1
-nome=input('Digite o nome da pessoa ') # entrada de dados
-peso=float(input('Digite o peso da pessoa '))
-altura=float(input('Digite a altura da pessoa '))
-
-imc=peso/altura**2 #calcula imc
-
-if (imc>22 or imc<20): # apresenta resultado
-    print(nome,' , imc: ',imc)
-
-#pessoa 2
-nome=input('Digite o nome da pessoa ') # entrada de dados
-peso=float(input('Digite o peso da pessoa '))
-altura=float(input('Digite a altura da pessoa '))
-
-imc=peso/altura**2 #calcula imc
-
-if (imc>22 or imc<20): # apresenta resultado
+# Determinação do IMC e verificação da categoria
+imc = peso / altura ** 2  # calcula imc
+if imc > 22 or imc < 20:  # apresenta nome seguido do resultado
     print(nome, ' , imc: ', imc)
 
-#pessoa 3
-nome=input('Digite o nome da pessoa ') # entrada de dados
-peso=float(input('Digite o peso da pessoa '))
-altura=float(input('Digite a altura da pessoa '))
+# Dados da pessoa 2
+nome = input('Digite o nome da pessoa ')  # entrada de dados
+peso = float(input('Digite o peso da pessoa '))
+altura = float(input('Digite a altura da pessoa '))
 
-imc=peso/altura**2 #calcula imc
+# Determinação do IMC e verificação da categoria
+imc = peso / altura ** 2
+if (imc > 22 or imc < 20):
+    print(nome, ' , imc: ', imc)
 
-if (imc>22 or imc<20): # apresenta resultado
+# Dados da pessoa 3
+nome = input('Digite o nome da pessoa ')  # entrada de dados
+peso = float(input('Digite o peso da pessoa '))
+altura = float(input('Digite a altura da pessoa '))
+
+# Determinação do IMC e verificação da categoria
+imc = peso / altura ** 2
+if (imc > 22 or imc < 20):
     print(nome, ' , imc: ', imc)
