1af9954cf8790db00c9c8273aca7dffee6992b84
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-19 17:18:59 -0300

    Update calcIMC20pessoas.py

--- calcIMC20pessoas.py
+++ calcIMC20pessoas.py
@@ -1,17 +1,22 @@
+# Este scrip de Python lê a altura e o peso de 20 pessoas
+# Calcula e escreve o imc médio do grupo
 # Clayton J A Silva
-# 3abr202
-# Este scrip de Python lê a nota de um grupo de 30 alunos
+# 19ago2022
 
-N = 3  # define a constante do número de alunos
+N = 20  # define a constante do número de pessoas
 cont = 1  # inicializa o contador
 media = 0  # inicializa a variável média
 
-while cont <= N:  # leitura dos dados e soma das notas
+while cont <= N:  # leitura dos dados e soma dos imc individuais
+    print('\n')  # \n é o que se chama caractere de escape
+    # inserido na string significa 'quebra de linha'
     peso = float(input('Digita o peso '))
     altura = float(input('Digita a altura '))
-    media += peso/altura**2
+    imc = peso / altura ** 2
+    print('O IMC da ', cont, 'a pessoa é ', imc)
+    media += imc
     cont += 1
-media = media/N  # determinação da media
+media = media / N  # determinação da media
 
 #  saída de dados
-print('A media do IMC é: ', media)
+print('\nA media do IMC é: ', media)
