177734b3cd4fd55f1587a22e1a9806770035bfea
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-19 17:47:23 -0300

    Update calcSaldo.py

--- calcSaldo.py
+++ calcSaldo.py
@@ -1,20 +1,25 @@
+# Este scrip de Python lê o CPF, saldo inicial e número de operações em um dia de uma conta bancária
+# Em seguida lê o tipo e o valor de cada uma das operações
+# Escreve o saldo diário final após ler todas as operações do dia
 # Clayton J A Silva
-# 3abr202
-# Este scrip de Python lê a nota de um grupo de 30 alunos
+# 19ago2022
 
 cpf = input('Digite o cpf: ')
 saldo_ini = float(input('Digite o saldo inicial: '))
 N = int(input('Digite o número de operações do dia: '))
 
-saldo = saldo_ini
-for cont in range(N):  # leitura dos dados e soma das notas
+cont = 1
+saldoFinal = saldo_ini
+while cont <= N:  # leitura dos dados e soma das notas
+    print('\noperação ', cont)
     valor = float(input('Digita o valor da operação '))
     natureza = input('Digite a natureza da operação depósito (d) ou saque (s): ')
     if natureza == 'd':
-        saldo += valor
+        saldoFinal += valor
     else:
-        saldo -= valor
+        saldoFinal -= valor
+    cont += 1
 
 #  saída de dados
-print('O CPF do correntista é ', cpf)
-print('O saldo do dia é ', round(saldo, 2))
+print('\nO CPF do correntista é ', cpf)
+print('O saldo do dia é ', round(saldoFinal, 2))
