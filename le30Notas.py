9bdaac5ead3597e648754d4809d58880059072bc
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-19 17:03:41 -0300

    Update le30Notas.py

--- le30Notas.py
+++ le30Notas.py
@@ -1,15 +1,18 @@
-# Clayton J A Silva
-# 3abr202
 # Este scrip de Python lê a nota de um grupo de 30 alunos
+# Calcula e escreve a media aritmética
+# Clayton J A Silva
+# 19ago2022
 
-N = 3  # define a constante do número de alunos
+N = 30  # define a constante do número de alunos
 cont = 1  # inicializa o contador
 media = 0  # inicializa a variável média
 
 while cont <= N:  # leitura dos dados e soma das notas
     nota = float(input('Digita a nota '))
-    media += nota
+    media += nota # O Python admite a forma contraída da sentença de atribuição x = x + y, por x += y
+                  # A alternativa também funciona com sintaxe similar para os operadores -, *, /, //
     cont += 1
+
 media = media/N  # determinação da media
 
 #  saída de dados
