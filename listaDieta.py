9309b4a108c5be1c756a08ac67866abf6457272a
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-09-02 08:34:22 -0300

    Update listaDieta.py

--- listaDieta.py
+++ listaDieta.py
@@ -1,7 +1,14 @@
-#  O programa ilustra o uso de funções em Python
-#  Lê dados de uma pessoa
-# Recomenda programa de exercícios
-# Recomenda dieta
+#  O programa na linguagem Python lê e armazene os dados de um grupo
+#  de 30 pessoas: nome, idade, peso, altura.
+#  Para a idade de cada pessoa lida, o programa deverá recomendar exercícios do tipo A,
+#  para pessoas acima de 60 anos; do tipo B, para pessoas entre 40 e 60 anos;
+#  do tipo C, para pessoas entre 15 e 40 anos; e do tipo D, para crianças (idade inferior a 15 anos).
+#  Com base no peso e na altura, o programa deve calcular o IMC (dado por peso/altura^2),
+#  determinando as dietas: ‘magra’, IMC superior a 25; ‘normal’, IMC entre 19 e 25;
+#  e ‘gorda’, IMC inferior a 19. O programa deverá armazenar a dieta de cada pessoa e o respectivo IMC.
+# Ilustra o uso de lista
+# Atualizado em agosto/2022
+# ProfClayton J A Silva
 
 def exercicio(i):  # Determina o programa de exercícios em função da idade 'i'
     if i < 15:
@@ -37,9 +44,11 @@ for i in range(N):
     imc.append(indice(peso[i], altura[i]))
     if imc[i] < 19:
         print('dieta gorda')
-    elif imc[i] > 25:
+    elif imc[i] <= 25:
+        print('dieta normal')
+    else:
         print('dieta magra')
 
-print('nome   idade    programa      imc')
+# Apresentação na forma da tabela
 for i in range(N):
     print(nome[i], ' ', idade[i], ' ', programa[i], ' ', round(imc[i], 1))
