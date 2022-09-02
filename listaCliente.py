ae82237d9b2cbe47be47f3dcfc76eade4f84bc50
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-09-02 09:40:46 -0300

    Update listaCliente.py

--- listaCliente.py
+++ listaCliente.py
@@ -1,9 +1,11 @@
-# Programa cadastra os códigos de um grupo de clientes de uma loja
+# Programa cadastra os códigos de um grupo de clientes de uma loja.
 # Cadastra o valor das respectivas compras realizadas em um determinado mês.
 # O código 0 significa que não será cadastrado novo cliente.
 # Após os clientes serem cadastrados, o usuário poderá fazer uma consulta ao sistema,
 # digitando um valor qualquer.
 # O sistema deverá escrever os códigos dos clientes que realizaram compras acima desse valor.
+# Atualizado em agosto de 2022
+# Clayton J A Silva
 
 cliente = []
 valor = []
@@ -12,10 +14,9 @@ while cod != '0':
     cliente.append(cod)
     valor.append(float(input('Digite o valor das compras: ')))
     cod = input('Digite o código: ')
+
 # Consulta ao sistema
 valor_ref = float(input('Digite o valor de referência: '))
 print('Código de clientes que compraram acima da referência')
-for i in range(len(cliente)):
-    if valor[i] > valor_ref:
-        print(cliente[i])
-
+x = [cliente[i] for i in range(len(cliente)) if valor[i] > valor_ref] # Usando compreensão de lista
+print(x)
