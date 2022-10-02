f98b43f2d1dd8f3864949204e140ba8a88a4baed
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-10-02 11:59:14 -0300

    Update StringExercicio2.py

--- StringExercicio2.py
+++ StringExercicio2.py
@@ -1,5 +1,6 @@
 #2. Elaborar um script em Python que permita ler um texto digitado pelo usurário.
-#O programa deverá calcular o número de vogais presentes no texto digitado.
+# O programa deverá calcular o número de vogais presentes no texto digitado.
+# Atualizado em 2/10/22
 
 texto = input('Digite o texto: ')
 N = len(texto)
@@ -7,4 +8,4 @@ cont = 0 # Contador de letras do texto
 for i in range(N):
   if (texto[i]=='a')or(texto[i]=='e')or(texto[i]=='i')or(texto[i]=='o')or(texto[i]=='u'): # Testa somente as letras vogais minúsculas
     cont += 1
-print(cont)
+print('O número de vogais no texto é... ',cont)
