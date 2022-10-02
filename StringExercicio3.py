056a667db73955ebd4b592b67e931909bea7f978
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-10-02 12:12:25 -0300

    Update StringExercicio3.py

--- StringExercicio3.py
+++ StringExercicio3.py
@@ -1,7 +1,8 @@
-#3. Elaborar um script em Python que leia uma palavra digitada pelo usuário.
-#O programa deverá escrever a palavra digitada com a posição das letras invertidas.
+# 3. Elaborar um script em Python que leia uma palavra digitada pelo usuário.
+# O programa deverá escrever a palavra digitada com a posição das letras invertidas.
+# Atualizado em 2/10/22
 
 palavra = input('Digite a palavra: ')
 N = len(palavra)
 for i in range(N):
-  print(palavra[N-i-1]) # Escreve da última letra para a primeira
+  print(palavra[N-i-1], end="") # Escreve da última letra para a primeira
