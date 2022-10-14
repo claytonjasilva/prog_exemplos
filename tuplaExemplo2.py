6021b2f742c77a2bdc42b09d3c6d0ba3eb536e13
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-10-14 09:14:06 -0300

    Update tuplaExemplo2.py

--- tuplaExemplo2.py
+++ tuplaExemplo2.py
@@ -1,12 +1,11 @@
 # Elaborar um programa em Python que leia a matrícula de um grupo de N alunos.
 # Como a matrícula identifica de modo inequìvoco cada aluno,
-# após cadastrar a matrícula dos alunos, o programa não admitirá mudar o número os dados cadastrados.
+# após cadastrar a matrícula dos alunos, o programa não admitirá mudar a matrícula cadastrada.
+# Atualizaado em 14/10/2022
 
 N = int(input('Digite o numero de alunos matriculados: '))
-mat = []
+matricula = []
 for i in range(N):
-    mat.append(input('Digite a matricula: '))
-matricula = tuple(mat)
+    matricula.append(input('Digite a matricula: '))
+matricula = tuple(matricula)
 print(matricula)
-# mat[0] = 3
-# print(mat)
