83e977c2a0678c66af4c7294f32ee905aa699826
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-10-14 10:09:33 -0300

    Update tuplaExemplo3.py

--- tuplaExemplo3.py
+++ tuplaExemplo3.py
@@ -1,23 +1,36 @@
-# Elaborar um programa em Python que leia a matrícula de um grupo de N alunos.
-# Leia também as notas do grupo (AP1, AP2 e AC).
-# O programa deverá escrever a matrícula e a média de cada aluno.
+# Elaborar um programa em Python que leia a matrícula de um grupo de N alunos,
+# cadastrando-as em uma tupla. Leia também as notas do grupo [AP1, AP2 e AC],
+# adastrando-as em uma lista. O programa deverá determinar a média parcial (MP) de cada aluno,
+# considerando que MP = 0,4(AP1+AP2) + 0,2 AC.
+# Deverá cadastrar em uma tupla as matriculas, as notas e a média parcial de todos os alunos.
 
+# Leitura do número de alunos da turma
 N = int(input('Digite quantos alunos: '))
-mat = []
+
+# Inicialização das listas
+matricula = []
 ap1 = []
 ap2 = []
 ac = []
-nota = [ap1, ap2, ac]
 media = []
+
+# Leitura dos dados
+print('Leitura de matriculas')
+matricula = [input('Digite a matricula: ') for i in range(N)]
+tuple(matricula)
+print('Leitura de notas AP1')
+ap1 = [float(input('Digite a nota da ap1: ')) for i in range(N)]
+print('Leitura de notas AP2')
+ap2 = [float(input('Digite a nota da ap2: ')) for i in range(N)]
+print('Leitura de notas AC')
+ac = [float(input('Digite a nota da ac: ')) for i in range(N)]
+
+# Determinação das médias
+media = [0.4 * (ap1[i] + ap2[i]) + 0.2 * ac[i] for i in range(N)]
+nota = [ap1, ap2, ac]  # Criando uma lista de listas
+aluno = matricula, nota, media
+
+# Geração do relatório
+print('RELATÓRIO')
 for i in range(N):
-    mat.append(input('Digite a matricula: '))
-    ap1.append(float(input('Digite a nota da ap1: ')))
-    ap2.append(float(input('Digite a nota da ap2: ')))
-    ac.append(float(input('Digite a nota da ac: ')))
-    media.append(0.4 * (ap1[i] + ap2[i]) + 0.2 * ac[i])
-matricula = tuple(mat)
-#print(matricula)
-#print(nota)
-#print(media)
-for i in range(N):
-    print(matricula[i],' ',nota[0][i],' ',nota[1][i],' ',nota[2][i],' ',media[i])
+    print(matricula[i], ' ', nota[i], ' ', media[i])
