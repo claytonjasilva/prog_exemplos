50b2bb5c5f1654d68c20f263fe6b527fb1f45908
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-02-22 14:50:56 -0300

    Update pratica3chaves2.ino

--- pratica3chaves2.ino
+++ pratica3chaves2.ino
@@ -1,27 +1,27 @@
-#define I0 2; // entradas binárias LEDs
-#define I1 3;
-#define I2 4;
-#define S0 5; // saidas binárias LEDs
-#define S1 6;
-#define S2 7;
+#define I0 2 // define a macro relativa a entrada (pino) 2 - em cada pino um LED
+#define I1 3
+#define I2 4
+#define S0 5 // define a macro relativa a saida (pino) 5 - em cada pino um LED
+#define S1 6
+#define S2 7
 
 
 void setup(){
-   pinMode(I0,INPUT);
+   pinMode(I0,INPUT); // Configura os pinos I0, I1 e I2 como entrada
    pinMode(I1,INPUT);
    pinMode(I2,INPUT);
-   pinMode(S0,OUTPUT);
+   pinMode(S0,OUTPUT); // Configura os pinos S0, S1 e S2 como saida
    pinMode(S1,OUTPUT);
    pinMode(S2,OUTPUT);
 }
 
 void loop(){
-   int a, b , c;
-   a = digitalRead(I0);
+   int a, b , c; // Declara as variáveis internas a funcao loop 
+   a = digitalRead(I0); // Le a entrada e atribui o valor lido a variavel
    b = digitalRead(I1);
    c = digitalRead(I2);
-   digitalWrite(S0,!I0);
-   digitalWrite(S1,!I1);
-   digitalWrite(S2,!I2);
-   delay(3000);
+   digitalWrite(S0,!a); // Inverte o valor lógico lido na entrada e acende a respectiva saída
+   digitalWrite(S1,!b);
+   digitalWrite(S2,!c);
+   delay(5000); // Proporciona um atraso de 5000 milissegundos entre dois loops 
 }
