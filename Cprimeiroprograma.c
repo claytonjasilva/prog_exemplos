324a5b9dd5de010012588aba9851b0f6224b7a76
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-02-24 11:08:46 -0300

    Create Cprimeiroprograma.c

---
+++ Cprimeiroprograma.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* Referencia: Aprenda a Programar em C, C++ e C#, de Hickson, R.*/
+
+void main() /* Meu Primeiro Programa */
+{
+    printf("Ola! Agora vou conseguir aprender o C!\n");
+}
