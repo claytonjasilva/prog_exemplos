c11ca7fb1f33391c15447b2d9f69dd261ae69734
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-02-24 14:35:52 -0300

    Create Cescrevecaractere.c

---
+++ Cescrevecaractere.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* Referencia: Aprenda a Programar em C, C++ e C#, de Hickson, R.*/
+
+int main()
+{
+    char Ch;
+    Ch='D';
+    printf("%c\n",Ch);
+    printf("%d",Ch);
+    return(0);
+}
