5af4c92a634709d9e4dbe0be50f959e3bbdfe4df
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-02-24 14:48:35 -0300

    Create Cusoconio.c

---
+++ Cusoconio.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+/* Referencia: Aprenda a Programar em C, C++ e C#, de Hickson, R.*/
+int main()
+{
+    char Ch;
+    Ch=getch();
+    printf("Voce pressionou a tecla %c",Ch);
+    return(0);
+}
