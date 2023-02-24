fb2a78cfde2ced0973fd418fa26e2484b064d1df
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-02-24 12:16:48 -0300

    Create Cprimeirafuncao.c

---
+++ Cprimeirafuncao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Referencia: Aprenda a Programar em C, C++ e C#, de Hickson, R.*/
+void mensagem()
+{
+    printf("Ola! ");
+}
+
+void main()
+{
+    mensagem();
+    printf("Agora vou conseguir aprender o C!\n");
+}
