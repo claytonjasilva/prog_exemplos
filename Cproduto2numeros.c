66e7633eafc8ec4c4ed91435ee17781c4abe36d6
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-02-24 12:34:09 -0300

    Create Cproduto2numeros.c

---
+++ Cproduto2numeros.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* Referencia: Aprenda a Programar em C, C++ e C#, de Hickson, R.*/
+int prod(int x,int y)
+{
+    return(x*y);
+}
+
+void main()
+{
+    int saida;
+    saida=prod(12,7);
+    printf("A saida e: %d\n",saida);
+}
