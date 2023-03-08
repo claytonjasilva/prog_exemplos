64177f5a97bcdd26ab97062d80aa64f1bf9e6510
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-08 17:04:30 -0300

    Create cursoC1102a.c

---
+++ cursoC1102a.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Nome        : Exemplos1_10.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	float ap1, ap2, ac; // Declara variáveis de ponto flutuante
+	float media;
+	media = 0.4*(8.5+7.3)+0.2*5; //Atribui o resultado da expressao para media
+	printf("%f",media); // Formato printf(<cadeia de caracteres de controle>,<lista de argumentos>);
+}
