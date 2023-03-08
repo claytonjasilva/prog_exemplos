fc7a33dd232d02b14ebc31e0e8288aadc9065cae
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-08 16:16:35 -0300

    Create curso110b.c

---
+++ curso110b.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Nome        : Exemplos1_10.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	printf("Agora vou iniciar...\n"); // Usando caractere de escape
+	printf("produzindo um novo codigo");
+}
