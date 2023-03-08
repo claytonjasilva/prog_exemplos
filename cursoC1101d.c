8c67c9928aa410ffdf979289adbb1cb51c098d0b
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-08 16:54:53 -0300

    Create cursoC1101d.c

---
+++ cursoC1101d.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Nome        : Exemplos1_10.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <math.h> //Biblioteca com funções matemáticas
+
+main() {
+	printf("%f",pow(3.0,4*5*(2*(4+6)))); // Formato printf(<cadeia de caracteres de controle>,<lista de argumentos>);
+}
