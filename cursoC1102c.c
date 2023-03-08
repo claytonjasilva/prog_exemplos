5eff95437ee35f0cf200988d26d80de4ad565b65
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-08 17:13:00 -0300

    Create cursoC1102c.c

---
+++ cursoC1102c.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Nome        : Exemplos1_10.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#define PI 3.1415
+
+main() {
+	float raio=2.1, area; // Declara variáveis de ponto flutuante
+	                      // Inicializa o valor de raio na declaracao
+	area = PI*raio*raio; //Atribui o resuultado da expressao para media
+	printf("%f",area); // Formato printf(<cadeia de caracteres de controle>,<lista de argumentos>);
+}
