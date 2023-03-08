f0e6cc327cd41b82c2efea71f58bf9f221c4c0cb
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-08 16:27:05 -0300

    Create cursoC1101c.c

---
+++ cursoC1101c.c
@@ -0,0 +1,15 @@
+/*
+ ============================================================================
+ Nome        : Exemplos1_10.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	printf("%i",3+4*5-(2*(4+6))); // Formato printf(<cadeia de caracteres de controle>,<lista de argumentos>);
+}
