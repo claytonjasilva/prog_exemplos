79a339d7f754e6d4b2e8dcd694e02b350395d5f5
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-08 21:37:10 -0300

    Create cursoC25d.c

---
+++ cursoC25d.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Nome        : Exemplos2_5.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	char ch1, ch2, ch3, ch4, ch5; // Declara variáveis de ponto flutuante
+	scanf("%c %c %c %c %c",&ch1, &ch2, &ch3, &ch4, &ch5);
+	printf("O primeiro caractere e %d\n",ch1);
+	printf("O segundo caractere e %d\n",ch2);
+	printf("O terceiro caractere e %d\n",ch3);
+	printf("O quarto caractere e %d\n",ch4);
+	printf("O quinto caractere e %d\n",ch5);
+}
