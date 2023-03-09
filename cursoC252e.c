073524f6de8858a5a93a1cb83d63a1cc2cf9ea4d
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-09 08:49:13 -0300

    Create cursoC252e.c

---
+++ cursoC252e.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Nome        : Exemplos2_5.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	char nome[20]; // Declara a string
+	printf("Digite o nome\n");
+	scanf("%s",nome); // Le o nome - nao usar o operador unario, pois o nome da string ja e um ponteiro
+	printf("Meu nome e %s",nome);
+}
