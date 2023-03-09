02725ccbfb97f7bbc15b58cc976c46711fc8f6fe
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-09 09:36:36 -0300

    Create cursoC351a.c

---
+++ cursoC351a.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Nome        : Exemplos3_5.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	int resultado, x=7; // Declara a variável ponteiro
+	resultado = (2 > x) && (3 + 4 == 6);
+	printf("O resultado da expressao logica e %i",resultado); 
+}
