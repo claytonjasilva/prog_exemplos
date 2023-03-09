87d6643c9db9a429bdb36564f596877a8a0d8ff8
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-08 21:15:41 -0300

    Update cursoC25a.c

--- cursoC25a.c
+++ cursoC25a.c
@@ -15,11 +15,11 @@ main() {
 	float a, b, c; // Declara variáveis de ponto flutuante
 	float delta, x1, x2;
 	printf("Digite os coeficientes\n");
-	scanf("%f %f %f",&a,&b,&c); // Le os coeficientes
+	scanf("%f %f %f",&a,&b,&c);
 	delta = b * b - 4 * a * c;
 	printf("delta e %f\n",delta);
-	x1 = (- b + sqrt(delta))/(2 * a); // Determina a raiz 1
-	x2 = (- b - sqrt(delta))/(2 * a); // Determina a raiz 2
-	printf("A raiz x1 e %f\n",x1); 
-	printf("A raiz x2 e %f\n",x2); 
+	x1 = (- b + sqrt(delta))/(2 * a);
+	x2 = (- b - sqrt(delta))/(2 * a);
+	printf("A raiz x1 e %4.2f\n",x1); // Formato printf(<cadeia de caracteres de controle>,<lista de argumentos>);
+	printf("A raiz x2 e %4.2f\n",x2); // Especifica o tamanho do campo e a precisão
 }
