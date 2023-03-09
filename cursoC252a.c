4aac2f398caf662465f6bc94e8008baacd87d800
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-08 22:01:25 -0300

    Create cursoC252a.c

---
+++ cursoC252a.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Nome        : Exemplos2_5.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <conio.h> // Inclui funcoes de leitura/escrita de caracteres
+
+main() {
+	char ch1, ch2, ch3, ch4, ch5; // Declara variáveis de ponto flutuante
+	ch1=getch(); // O prototipo da funcao e int getch(void);
+	ch2=getch();
+	ch3=getch();
+	ch4=getch();
+	ch5=getch();
+	putchar(ch1);
+	putchar(ch2);
+	putchar(ch3);
+	putchar(ch4);
+	putchar(ch5);
+}
