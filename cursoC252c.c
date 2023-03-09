0536c0cfe21c04b82b4f499c0859ed51f62e8496
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-09 08:22:49 -0300

    Create cursoC252c.c

---
+++ cursoC252c.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Nome        : Exemplos2_5.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <conio.h> // Inclui funcoes de leitura/escrita de caracteres
+
+main() {
+	char meu_nome[20]="clayton j a silva"; // Declara e inicializa a string
+	printf("Meu nome e %s",meu_nome);
+}
