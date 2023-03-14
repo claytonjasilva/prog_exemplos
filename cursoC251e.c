118524b6ac26784360d1ba081cd8633e4054ff7e
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-14 17:35:40 -0300

    Create cursoC251e.c

---
+++ cursoC251e.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Nome        : Exemplos2_5.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	char ch1='A', ch2='E', ch3='I', ch4='O', ch5='U'; // Declara e inicializa variáveis do tipo char
+	// Observe que as variáveis do tipo char sao inscritas entre aspas simples
+	printf("%c, %c, %c, %c, %c",ch1,ch2,ch3,ch4,ch5);
+	
+}
