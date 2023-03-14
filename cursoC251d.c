70738452b4cd328c1f12fdc306dbd9cbb6d73419
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-14 17:48:02 -0300

    Update cursoC251d.c

--- cursoC251d.c
+++ cursoC251d.c
@@ -13,9 +13,9 @@
 main() {
 	char ch1, ch2, ch3, ch4, ch5; // Declara variáveis de ponto flutuante
 	scanf("%c %c %c %c %c",&ch1, &ch2, &ch3, &ch4, &ch5);
-	printf("O primeiro caractere e %d\n",ch1);
-	printf("O segundo caractere e %d\n",ch2);
-	printf("O terceiro caractere e %d\n",ch3);
-	printf("O quarto caractere e %d\n",ch4);
-	printf("O quinto caractere e %d\n",ch5);
+	printf("O primeiro caractere e %c\n",ch1);
+	printf("O segundo caractere e %c\n",ch2);
+	printf("O terceiro caractere e %c\n",ch3);
+	printf("O quarto caractere e %c\n",ch4);
+	printf("O quinto caractere e %c\n",ch5);
 }
