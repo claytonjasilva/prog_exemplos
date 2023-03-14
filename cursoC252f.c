c0e69a0d50170fdacf8e7d5b2c51a173b4024bcc
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-14 20:59:58 -0300

    Update cursoC252f.c

--- cursoC252f.c
+++ cursoC252f.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 
 main() {
-	char *nome="clayton j a silva"; // Declara a variável ponteiro
+	char *nome; // Declara a variável ponteiro de caractere
+	nome="clayton j a silva"; // Atenção!!! string declarada como vetor nao aceita comando de atribuicao
 	printf("A quinta letra do meu nome e %c",nome[4]); // Atencao que o especificador e %c. Como um vetor, o primeiro caractere e referenciado pelo indice [0]
 }
