0386a47f5a49c1d0151d5559b7070a26b16c6a8b
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-14 20:07:24 -0300

    Create curso252d.c

---
+++ curso252d.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Nome        : Exemplos2_5.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	char nome[20]; // Declara a string que pode ter até 20 caracteres
+	printf("Digite o nome\n");
+	scanf("%s",nome); // Observe o especificador de tipo %s 
+                    // Nao usar o operador unario &, pois o nome da string ja e um ponteiro
+	printf("Meu nome e %s",nome);
+}
