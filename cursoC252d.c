15eb0607bdb5a1fd5dcc890a93f1a4c9879b6827
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-15 18:30:12 -0300

    Update cursoC252d.c

--- cursoC252d.c
+++ cursoC252d.c
@@ -13,6 +13,6 @@
 main() {
 	char nome[20]; // Declara a variavel 
 	printf("Digite o primeiro nome da pessoa: ");
-	scanf("%s",nome);
+	scanf("%s",nome); // Observe que não se utiliza o especificador de tipo na leitura
 	printf("O nome digitado eh: %s",nome);
 }
