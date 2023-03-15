c2940ba7176083b266027445e52d86a6ac79a2ca
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-15 17:57:35 -0300

    Create cursoC252d.c

---
+++ cursoC252d.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Nome        : Exemplos3_5.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	char nome[20]; // Declara a variavel 
+	printf("Digite o primeiro nome da pessoa: ");
+	scanf("%s",nome);
+	printf("O nome digitado eh: %s",nome);
+}
