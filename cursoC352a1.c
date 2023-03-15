338d8964a902758af1cc3bc9d2a36700240b2340
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-03-15 17:50:13 -0300

    Create cursoC352a1.c

---
+++ cursoC352a1.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Nome        : Exemplos3_5.c
+ Autor      : Prof Clayton J A Silva
+ Versao     :
+ Copyright   : Curso de C Ibmec
+ Descricao : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+main() {
+	float a, b; // Declara as variaveis
+	printf("Digite dois numeros diferentes: \n");
+	leitura: scanf("%f %f",&a,&b); // leitura e um label utilizado combinado com o comando goto
+	if (a==b){
+		printf("Os numeros digitados sao iguais! Digite novamente dois numeros diferentes...\n");
+		goto leitura; // O comando goto nao eh muito recomendado, pois desestrutura o codigo,
+		              // porem e util para TRATAMENTO DE EXCEÇOES
+	}
+	(a>b)? printf("O maior numero e %f",a):printf("O maior numero e %f",b); // Uso do operador ?
+}
