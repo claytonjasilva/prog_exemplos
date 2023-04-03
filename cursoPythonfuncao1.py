3307a60ab63561d1ce44806611b5416db4cd4e7a
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-04-02 23:18:11 -0300

    Create cursoPythonfuncao1.py

---
+++ cursoPythonfuncao1.py
@@ -0,0 +1,51 @@
+# Elaborar um script em Python para ler um número inteiro e uma dentre 4 opções de operação:
+# fatorial, primos, soma e pares.
+# Se a opção dada for fatorial, o script deve escrever o fatorial do número.
+# Se for primos, escrever os primos inferiroes ao número lido.
+# Se for soma, escrever a soma dos números inferiores não negativos.
+# Se for pares, escrever todos os pares inferiores ao número.
+
+def fatorial(numero):
+    if numero == 1:
+        return 1
+    else:
+        return numero * fatorial(numero - 1)
+
+
+def primo(numero):
+    flag = True
+    for i in range(2, numero):
+        if numero % i == 0:
+            flag = False
+            break
+    return flag
+
+
+def soma(numero):
+    tot = 0
+    for i in range(numero):
+        tot += i
+    return tot
+
+
+def par(numero):
+    tot = 0
+    for i in range(2, numero, 2):
+        tot += i
+    return tot
+
+
+N = int(input('Digite o numero: '))
+opcao = input('Digite a opcao: ')
+match opcao:
+    case 'fatorial':
+        print(fatorial(N))
+    case 'primo':
+        if primo(N):
+            print('primo')
+        else:
+            print('nao primo')
+    case 'soma':
+        print(soma(N))
+    case 'par':
+        print(par(N))
