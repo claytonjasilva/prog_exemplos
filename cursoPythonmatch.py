538a091fa11937982b77215430af60ade4085cb3
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-04-05 17:06:47 -0300

    Update cursoPythonmatch.py

--- cursoPythonmatch.py
+++ cursoPythonmatch.py
@@ -5,26 +5,29 @@
 # Escrever o percentual dos animais velhos.
 # Interromper a leitura quando o usuário digitar idade igual a 0.
 
-cont = 0
-cont_jovem = 0
-cont_velho = 0
-maior_peso = 0
-soma = 0
+cont = 0 # Inicializa variável que conta os animais com idades válidas
+cont_jovem = 0 # Inicializa variável que conta os animais jovens
+soma = 0 # Inicializa a variável que armazena a soma dos pesos dos animais jovens
+maior_peso = 0 # Inicializa a variável que armazena o mairo pesso dos animais adultos
+cont_velho = 0 # Inicializa variável que conta os animais velhos
 idade = int(input('Digite a idade do animal: '))
 while idade != 0:
-    cont += 1
+  if idade == 0:
+    break
+  else:
+    cont += 1 # Incrementa a variável que conta os animias como idades válidas
     classe = input('Digite a classe do animal: ')
     peso = float(input('Digite o peso do animal '))
     match classe:
         case 'J':
-            soma += idade
-            cont_jovem += 1
+            soma += idade # Atualiza a soma dos pesos dos animais jovens
+            cont_jovem += 1 # Atualiza a contagem do número de animias jovens
         case 'A':
-            if peso > maior_peso:
+            if peso > maior_peso: #troca e armazena peso e nome dos animias adultos mais pesados 
                 maior_peso = peso
                 maior_idade = idade
         case 'V':
-            cont_velho += 1
+            cont_velho += 1 # Atualiza a contagem do número de animias velhos
     idade = int(input('Digite a idade do animal: '))
 
 if cont == 0:
@@ -35,5 +38,11 @@ else:
         print('A media das idades dos animais jovens eh', media_jovem, ' anos.')
     else:
         print('Nao ha animais jovens.')
-    print('A idade do animal adulto mais pesado eh ', maior_idade)
-    print('O percentual de animais velhos eh ', cont_velho * 100 / cont, ' %')
+    if maior_peso == 0:
+      print('Não foi digitado dado de animal adulto')
+    else:
+      print('A idade do animal adulto mais pesado eh ', maior_idade)
+    if cont_velho == 0:
+      print('Não foi digitado dado de animal velho')
+    else:
+      print('O percentual de animais velhos eh ', cont_velho * 100 / cont, ' %')
