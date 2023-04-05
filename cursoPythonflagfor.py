670de5f0bcd33a781ac39681fb60e48ef0934199
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-04-05 16:26:45 -0300

    Update cursoPythonflagfor.py

--- cursoPythonflagfor.py
+++ cursoPythonflagfor.py
@@ -1,20 +1,21 @@
-# Elaborar um script em Python que leia o nome e a idade de um grupo de 50 animais.
+# Elaborar um script em Python que leia o nome e a idade de um grupo de 50 animais. 
 # Escrever o nome do animal mais velho.
 # Interromper a leitura se o usuário digitar idade igual a 0.
 
-maior_idade = 0
-N = 50
+N = 50 # Define a constante que limita a contagem
+maior_idade = 0 # Inicializa a variável que armazena a maior idade
 idade = int(input('Digite a idade do animal: '))
-for i in range(50):
-    nome = input('Digite o nome do animal: ')
-    if idade > maior_idade: # troca
+for cont in range(50):
+    if idade == 0:
+      break
+    else:
+      nome = input('Digite o nome do animal: ')
+      if idade > maior_idade:
         maior_idade = idade
         maior_nome = nome
-    idade = int(input('Digite a idade do animal: '))
-    if idade == 0:
-        break
+      idade = int(input('Digite a idade do animal: '))
 
-if maior_idade == 0:
+if cont == 0:
     print('Não foi digitado idade e nome de nenhum animal')
 else:
-    print('O animal mais velho possui ', maior_idade, ' anos. Seu nome eh ', maior_nome)
+    print('O nome do animal mais velho dentre os ',cont, ' animais eh ',maior_nome)
