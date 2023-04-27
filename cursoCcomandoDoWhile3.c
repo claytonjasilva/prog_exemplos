c041aa28eb709e79fbbb43d873233b7a6828592e
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-04-26 21:34:07 -0300

    Create cursoCcomandoDoWhile3.c

---
+++ cursoCcomandoDoWhile3.c
@@ -0,0 +1,65 @@
+/*Elaborar um código em C para ler um número inteiro *n* positivo. O usuário deve escolher uma opção de 1 a 3.  
+A opção 1 deve escrever a soma dos *n* termos da série S = 1/(k+3), k=0,...,n-1  
+A opção 2 deve escrever a soma dos *n* termos da série S = 2k/(k+1), k=0,...,n-1  
+A opção 3 deve escrever a soma dos *n* termos da série S = k, k=0,...,n-1  
+Se o usuário digitar uma opção inválida o programa deve permanecer solicitando uma opção válida.*/
+
+#include <stdio.h>
+
+float opcao1(N){
+    float S = 0, termo;
+    for (int i=0;i<N;i++){
+        termo = 1/(i + 3);
+        S += termo;
+    }
+    return S;
+}
+
+
+float opcao2(N){
+    float S = 0, termo;
+    for (int i=0;i<N;i++){
+        termo = (2 *i)/(i  + 1);
+        S += termo;
+    }
+    return S;
+}
+
+int opcao3(N){
+    int S = 0;
+    for (int i=0;i<N;i++)
+        S += i;
+    return S;
+}
+
+
+int main () {
+    int numero, flag = 1;
+    float resultado;
+    char opcao;
+    printf("Digite o numero inteiro positivo: ");
+    scanf("%d",&numero);
+    do {
+        printf("Digite uma opcao - 1, 2 ,3: \n");
+        scanf(" %c",&opcao); // Limpando o buffer de teclado com um espaço em branco antes do %c
+        if (opcao != '1' && opcao != '2' && opcao != '3')
+            continue;
+        switch (opcao) {
+            case '1':
+                resultado = opcao1(numero);
+                flag = 0;
+                break;
+            case '2':
+                resultado = opcao2(numero);
+                flag = 0;
+                break;
+            case '3':
+                resultado = opcao3(numero);
+                flag = 0;
+                break;    
+        }
+    } while (flag);
+    printf("O resultado da operacao eh %.5f",resultado);
+    return 0;
+}
+
