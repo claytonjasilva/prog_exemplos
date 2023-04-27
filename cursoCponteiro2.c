7652e09275f71d90e020ad81d17426fd4f1bc229
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2023-04-26 22:31:28 -0300

    Create cursoCponteiro2.c

---
+++ cursoCponteiro2.c
@@ -0,0 +1,26 @@
+/* Escrever um código em C para ler e armazenar 10 números de ponto flutuante.  
+Escrever cada um dos números lidos seguidos da média. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    float media;
+    float *p;
+    p = (float *) malloc(10*sizeof(float)); // Aloca 10 dinamicamente endereços de float 
+    for (int i = 0;i<10;i++){
+        printf("\nnumero: ");
+        scanf("%f",p);
+        media = *p;
+        p++;
+    }
+    media = media / 10;
+    p -= 10; // Retorna ao primeiro endereço dos números de ponto flutuante
+    for (int i = 0;i<10;i++){
+        printf("%f e %f\n",*p, media);
+        p++; // Incrementa para o próximo endereço do dado lido
+    }
+
+    return 0;
+}
+
