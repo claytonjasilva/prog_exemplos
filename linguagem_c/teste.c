5666c32f9df7137f31335d8c0c39bbdddbe63fa0
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2024-09-20 16:31:50 -0300

    ajustes

---
+++ linguagem_c/teste.c
@@ -0,0 +1,19 @@
+/*
+ Barra e asterisco inicia comentário de  mais de uma linha
+ ============================================================================
+ Nome        : Exemplos1.c
+ Autor      : Prof Clayton J A Silva
+ Copyright   : Curso de C Ibmec
+ Descrição : Meu primeiro programa em C
+ ============================================================================
+ Asterisco e barra fecha comentário aberto
+ */
+
+#include <stdio.h> /*diretiva de pré-processamento, inclui arquivo cabeçalho da biblioteca padrão */
+
+int  main(){ //função principal - retorna inteiro
+
+    printf("Meu primeiro programa em C"); //printf é uma função de saída da biblioteca stdio.h
+
+    return 0; //valor de retorno da função principal
+} //bloco de instruções delimitado por {}
